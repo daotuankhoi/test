5ff71f23c51cce9f6455d0412e9ac10b1e1f6522
daotuankhoi <106742275+daotuankhoi@users.noreply.github.com>
2023-06-03 17:23:28 +0700

    Add files via upload

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //dhfahfdigfnghidshidsfigdfhhfdgikjghfdghdkgh
+        print("Hello World!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        print("nfhxuvcufoifdguhsiughdiugigyifdhuc ycuvd ");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
